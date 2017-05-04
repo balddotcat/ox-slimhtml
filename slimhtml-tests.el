660df26b3d56a22171e439d01557cfa2e8fd8c4e
Elo Laszlo <laszlo@manifold.io>
2017-05-04 17:44:27 -0400

    add newline to default test function

--- slimhtml-tests.el
+++ slimhtml-tests.el
@@ -6,26 +6,24 @@
 
 
 (defun should-render-as (expected-result org-source &optional info)
-  (should (string= (concat expected-result "\n") (org-export-string-as org-source 'slimhtml t info))))
+  (setq expected-result (concat expected-result "\n"))
+  (should (string= expected-result (org-export-string-as org-source 'slimhtml t info))))
 
 
 (ert-deftest slimhtml-bold ()
   (should-render-as "<p><strong>this</strong></p>" "*this*"))
 
 (ert-deftest slimhtml-export-block ()
-  :expected-result :failed
-  (should-render-as "<img src=\"this.png\"\\>\n"
+  (should-render-as "<img src=\"this.png\"\\>"
                     "#+BEGIN_HTML\n<img src=\"this.png\"\\>\n#+END_HTML"))
 
 (ert-deftest slimhtml-export-snippet ()
-  :expected-result :failed
-  (should-render-as "<img src=\"this.png\"\\>"
+  (should-render-as "<p><img src=\"this.png\"\\></p>"
                     "@@html:<img src=\"this.png\"\\>@@"))
 
 (ert-deftest slimhtml-headline ()
-  :expected-result :failed
   (should-render-as "<h1>this</h1>" "* this\n")
-  (should-render-as "<h1>this</h1>"
+  (should-render-as "<h1 class=\"this\">headline</h1>"
                     "* headline\n :PROPERTIES:\n:attr_html: :class this\n:END:\n"))
 
 (ert-deftest slimhtml-inner-template ()
@@ -40,8 +38,7 @@
   (should-render-as "<p><em>this</em></p>" "/this/"))
 
 (ert-deftest slimhtml-item ()
-  :expected-result :failed
-  (should-render-as "<li>this\n</li>" " - this"))
+  (should-render-as "<ul><li>this\n</li>\n</ul>" "\n - this"))
 
 (ert-deftest slimhtml-link ()
   :expected-result :failed
@@ -57,40 +54,35 @@
                     "[[https://this.org][http://contents.org]]"))
 
 (ert-deftest slimhtml-paragraph ()
-  :expected-result :failed
   (should-render-as "<p>this</p>" "this")
   (should-render-as "<p class=\"this\">paragraph</p>"
                     "#+attr_html: :class this\nparagraph")
-  (should-render-as "<li>list item\nthis\n</li>"
+  (should-render-as "<ul><li>list item\nthis\n</li>\n</ul>"
                     "- list item\n  this")
   (should-render-as "<style>#this{color:#f73;}\n</style>"
                     "#+BEGIN_STYLE\n#this{color:#f73;}\n#+END_STYLE"))
 
 (ert-deftest slimhtml-plain-list ()
-  :expected-result :failed
-  (should-render-as "<ul><li>this\n</li>\n</ul>\n" " - this")
-  (should-render-as "<ul class=\"this\"><li>item\n</li>\n</ul>\n"
+  (should-render-as "<ul><li>this\n</li>\n</ul>" " - this")
+  (should-render-as "<ul class=\"this\"><li>item\n</li>\n</ul>"
                     "#+attr_html: :class this\n - item"))
 
 (ert-deftest slimhtml-plain-text ()
-  :expected-result :failed
   (should-render-as "<p>&amp;&lt;&gt;</p>" "&<>"))
 
 (ert-deftest slimhtml-section ()
   (should-render-as "<p>this</p>" "this"))
 
 (ert-deftest slimhtml-special-block ()
-  :expected-result :failed
   (should-render-as "<style>#this{color:#f73;}\n</style>"
                     "#+BEGIN_STYLE\n#this{color:#f73;}\n#+END_STYLE")
   (should-render-as "<style type=\"text/css\">#id{color:#f73;}\n</style>"
                     "#+attr_html: :type text/css\n#+BEGIN_STYLE\n#id{color:#f73;}\n#+END_STYLE"))
 
 (ert-deftest slimhtml-src-block ()
-  :expected-result :failed
-  (should-render-as "<code class=\"lisp\"><pre>(message 'this)</pre></code>"
+  (should-render-as "<code class=\"lisp\"><pre>(message 'this)\n</pre></code>"
                     "#+BEGIN_SRC lisp\n  (message 'this)\n#+END_SRC")
-  (should-render-as "<code class=\"sh\"><pre>&amp;&lt;&gt;</pre></code>"
+  (should-render-as "<code class=\"sh\"><pre>&amp;&lt;&gt;\n</pre></code>"
                     "#+BEGIN_SRC sh\n  &<>\n#+END_SRC"))
 
 (ert-deftest slimhtml-template ()
@@ -102,17 +94,16 @@
                                         (format "<article>%s</article>" contents)))))
 
 (ert-deftest slimhtml-verbatim ()
-  :expected-result :failed
-  (should-render-as "<kbd>this</kbd>" "=this="))
+  (should-render-as "<p><kbd>this</kbd></p>" "=this=")
+  (should-render-as "<p><kbd>&amp;&lt;&gt;</kbd></p>" "=&<>="))
 
 
 (ert-deftest slimhtml:attr ()
-  :expected-result :failed
   (should-render-as "<h1 class=\"this\">headline</h1>"
                     "* headline\n :PROPERTIES:\n:attr_html: :class this\n:END:\n")
   (should-render-as "<p class=\"this\">paragraph</p>"
                     "#+attr_html: :class this\nparagraph")
-  (should-render-as "<ul class=\"this\"><li>item\n</li>\n</ul>\n"
+  (should-render-as "<ul class=\"this\"><li>item\n</li>\n</ul>"
                     "#+attr_html: :class this\n - item")
   (should-render-as "<style type=\"text/css\">#id{color:#f73;}\n</style>"
                     "#+attr_html: :type text/css\n#+BEGIN_STYLE\n#id{color:#f73;}\n#+END_STYLE"))
